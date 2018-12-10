ff362d0bf3568c2fa05e3d7b34e0b40b3779f3fd
Bailly <jeremy.bailly@hotmail.com>
2018-12-10 15:17:36 +0100

    [ADD] list heroes name

--- src/Main.js
+++ src/Main.js
@@ -4,6 +4,23 @@ import './Main.css';
 import SuperheroElement from './components/SuperheroElement';
 
 class Main extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { listCharac: [] 
+    };
+  }
+
+  getAllHeroes() {
+    fetch(process.env.REACT_APP_API_URL + '/v1/public/characters' + '?apikey=' + process.env.REACT_APP_PUBLIC_KEY).then(responseJson => 
+       responseJson.json()
+  ).then(json => 
+    this.setState({
+      listCharac: json.data.results
+    })
+  );
+  }
+
   render() {
     return (
       <div className="Main">
@@ -15,8 +32,12 @@ class Main extends Component {
 
         <SuperheroElement />
         <SuperheroElement />
-
+        {this.getAllHeroes()}
+        {this.state.listCharac.map((charac, index) =>(
+          <td>{charac.name}</td>
+        ))}
       </div>
+      
     );
   }
 

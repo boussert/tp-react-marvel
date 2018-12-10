ef347acd56e3e98cfec61e5ec533dc8597d7d416
lpiem <morgane0798@hotmail.fr>
2018-12-10 16:15:38 +0100

    [WIP] Liste superheros

--- src/components/SuperheroElement.js
+++ src/components/SuperheroElement.js
@@ -2,27 +2,25 @@ import React, { Component } from 'react';
 import '../Main.css';
 import { Link } from 'react-router-dom'
 
-class SuperheroElement extends Component {
-
-  render() {
+function SuperheroElement(props) {
 
     return (
       <div className="ui card">
         <div className="image">
-          <img src="https://fsmedia.imgix.net/67/9f/12/f7/dfb0/4f50/b313/824e861ed92f/the-hulk-thanos.jpeg?rect=0%2C7%2C780%2C390&dpr=2&auto=format%2Ccompress&w=650" />
+          <img src={props.image} />
         </div>
 
         <div className="content">
-          <a className="header">Hulk</a>
+          <a className="header">{props.nom}</a>
         </div>
 
         <div className="extra content">
-          <button><Link to="/hero/$250" replace >Voir le détail</Link></button>
+          <button><Link to="/hero/${props.id}" replace >Voir le détail</Link></button>
         </div>
 
       </div>
     );
-  }
+
 
 }
 
